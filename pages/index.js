fe577045e0de81b2d5c4114c442c7ea05fb22f95
Diego Sadock <95287173+diegosadock@users.noreply.github.com>
2024-06-29 02:54:17 +0000

    add `home-style.module.css` file

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,14 @@
 import React, { useState, useEffect } from "react";
+import styles from "./home-style.module.css";
 
 function Home() {
   const [realAmount, setRealAmount] = useState("");
   const [euroAmount, setEuroAmount] = useState("");
-  const [euroAsk, setEuroAsk] = useState(null); // State for Euro ask value
+  const [euroAsk, setEuroAsk] = useState(null);
   const [realAsk, setRealAsk] = useState(null);
-  const [error, setError] = useState(null); // State for potential errors
+  const [error, setError] = useState(null);
+  const [isEuroToReal, setIsEuroToReal] = useState(true);
 
-  // Fetch Euro ask value on component mount
   useEffect(() => {
     const fetchData = async () => {
       try {
@@ -23,46 +24,88 @@ function Home() {
     fetchData();
   }, []);
 
-  const handleConvertToEuro = () => {
-    if (realAmount !== "" && realAsk !== null) {
-      const euroValue = parseFloat(realAmount) / parseFloat(euroAsk);
-      setRealAmount(euroValue.toFixed(2));
+  const handleConvert = () => {
+    if (isEuroToReal) {
+      if (euroAmount !== "" && euroAsk !== null) {
+        const realValue = parseFloat(euroAmount) * parseFloat(euroAsk);
+        setRealAmount(realValue.toFixed(2));
+      } else {
+        setError("Preencha o valor em Euro e aguarde a cotação ser carregada.");
+      }
     } else {
-      setError("Preencha o valor em Euro e aguarde a cotação ser carregada.");
-    }
-  };
-
-  const handleConvertToReal = () => {
-    if (euroAmount !== "" && euroAsk !== null) {
-      const realValue = parseFloat(euroAmount) * parseFloat(euroAsk);
-      setRealAmount(realValue.toFixed(2)); // Format to two decimal places
-    } else {
-      setError("Preencha o valor em Euro e aguarde a cotação ser carregada.");
+      if (realAmount !== "" && realAsk !== null) {
+        const euroValue = parseFloat(realAmount) / parseFloat(euroAsk);
+        setEuroAmount(euroValue.toFixed(2));
+      } else {
+        setError("Preencha o valor em Real e aguarde a cotação ser carregada.");
+      }
     }
   };
 
   return (
-    <div>
-      <h1>Conversor de Moedas</h1>
-      {error ? <p className="error">{error}</p> : null}
+    <div className={styles.home}>
+      <img
+        src="https://th.bing.com/th/id/OIG2.EhnL6ydxfGiyakmkjALM?pid=ImgGn"
+        alt="Guia de conversão de moedas"
+        className={styles.currencyGuideImage}
+      />
+      <h1>Conversor de Moedas 🌍</h1>
+      {error && <p className="error">{error}</p>}
       <input
         type="number"
-        placeholder="Valor em Euro (EUR)"
-        value={euroAmount}
-        onChange={(e) => setEuroAmount(e.target.value)}
+        placeholder={
+          isEuroToReal ? "Valor em Euro (EUR) 💶" : "Valor em Real (BRL)"
+        }
+        value={isEuroToReal ? euroAmount : realAmount}
+        onChange={(e) =>
+          isEuroToReal
+            ? setEuroAmount(e.target.value)
+            : setRealAmount(e.target.value)
+        }
       />
-      <button onClick={handleConvertToReal}>Converter para Real</button>
-      <p>Valor em Real: {realAmount}</p>
-
-      <input
-        type="number"
-        placeholder="Valor em Real (BRL)"
-        value={realAmount}
-        onChange={(e) => setRealAmount(e.target.value)}
-        disabled // Disable input for converted Real value
-      />
-      <button onClick={handleConvertToEuro}>Converter para Euro</button>
-      <p>Valor em Euro: {euroAmount}</p>
+      <button onClick={handleConvert}>
+        {isEuroToReal ? "Converter para Real" : "Converter para Euro 💶"}
+      </button>
+      <p className={styles.real}>
+        {isEuroToReal ? "Valor em Real:" : "Valor em Euro:"}{" "}
+        {isEuroToReal ? realAmount : euroAmount}
+      </p>
+      <button onClick={() => setIsEuroToReal(!isEuroToReal)}>
+        Trocar direção da conversão 🔃
+      </button>
+      <div className={styles.centro}>
+        <p>
+          <span className={styles.euroColor}>1 Euro</span> vale{" "}
+          <span className={styles.realColor}>
+            {parseFloat(euroAsk).toFixed(2)} Reais
+          </span>
+          <br></br>
+          <span className={styles.euroColor}>100 Euros</span> valem{" "}
+          <span className={styles.realColor}>
+            {parseFloat(euroAsk * 100).toFixed(0)} Reais
+          </span>
+          <br></br>
+          <span className={styles.euroColor}>500 Euros</span> valem{" "}
+          <span className={styles.realColor}>
+            {parseFloat(euroAsk * 500).toFixed(0)} Reais
+          </span>
+          <br></br>
+          <span className={styles.euroColor}>1000 Euros</span> valem{" "}
+          <span className={styles.realColor}>
+            {parseFloat(euroAsk * 1000).toFixed(0)} Reais
+          </span>
+          <br></br>
+          <span className={styles.euroColor}>3000 Euros</span> valem{" "}
+          <span className={styles.realColor}>
+            {parseFloat(euroAsk * 3000).toFixed(0)} Reais
+          </span>
+          <br></br>
+          <span className={styles.euroColor}>5000 Euros</span> valem{" "}
+          <span className={styles.realColor}>
+            {parseFloat(euroAsk * 5000).toFixed(0)} Reais
+          </span>
+        </p>
+      </div>
     </div>
   );
 }
